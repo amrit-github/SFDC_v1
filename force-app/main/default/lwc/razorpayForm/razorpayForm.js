74664555f82d9fd62443a441f883fd96657cf08f
amrit <amritsuman1996@gmail.com>
2025-06-29 15:44:48 +0530

    SFDC_Razorpay Integration

---
+++ force-app/main/default/lwc/razorpayForm/razorpayForm.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+import createOrder from '@salesforce/apex/RazorPayService.createOrder';
+
+export default class RazorpayForm extends LightningElement {
+    amount = 1;
+
+    handleAmountChange(event) {
+        this.amount = event.target.value;
+    }
+
+    async handlePay() {
+        try {
+            const payment = {
+                Amount__c: parseFloat(this.amount)
+            };
+
+            const checkoutUrl = await createOrder({ payment });
+            console.log('Opening Razorpay VF URL:', checkoutUrl);
+
+            window.open(checkoutUrl, '_blank');
+        } catch (error) {
+            console.error('Error creating order:', error);
+            alert('Failed to initiate payment.');
+        }
+    }
+}
